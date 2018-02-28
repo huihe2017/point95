11f736859646601ba2960e70ceaaab249aed0266
libw <3180234769@qq.com>
2018-02-28 10:15:29 +0800

    依赖修改

--- src/containers/home/homeSwiper/index.js
+++ src/containers/home/homeSwiper/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import style from "./index.css"
-
 import { Carousel } from 'antd';
 
-
-
-
-class ToolBar extends React.Component {
+class homeSwiper extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -14,32 +10,11 @@ class ToolBar extends React.Component {
         }
     }
     render() {
-
         return (
-            <div className={style.wrap}>
 
-                <Carousel autoplay>
-                    <div><h3>1</h3></div>
-                    <div><h3>2</h3></div>
-                    <div><h3>3</h3></div>
-                    <div><h3>4</h3></div>
-                </Carousel>
-            </div>
+            <div>1</div>
         )
     }
 }
 
-function mapStateToProps(state, props) {
-    return {
-
-    }
-}
-
-function mapDispatchToProps(dispatch) {
-    return {
-
-    }
-}
-
-
-export default ToolBar;
+export default homeSwiper;
