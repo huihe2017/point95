02ec153bee50b22c4895ca4210ccf14f1fe0954c
libw <3180234769@qq.com>
2018-03-26 15:29:13 +0800

    聊天

--- src/containers/checkUser/components/userChat/index.js
+++ src/containers/checkUser/components/userChat/index.js
@@ -1,31 +1,79 @@
 import ChatBot from 'react-simple-chatbot';
 import React from "react";
+import io from 'socket.io-client'
+import {Launcher} from 'react-chat-window'
 
-const steps = [
+
+const messageHistory = [
     {
-        id: '0',
-        message: 'Welcome to react chatbot!',
-        trigger: '1',
+        author: 'them',
+        type: 'text',
+        data: {
+            text: 'some text'
+        }
     },
+
     {
-        id: '1',
-        message: 'Bye!',
-        end: false,
-    },
+        author: 'me',
+        type: 'emoji',
+        data: {
+            code: 'someCode'
+        }
+    }
+
 ];
 
 class Chat extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            messageList : messageHistory
         }
     }
+
+    componentWillMount(){
+        let socket=io.connect("ws://192.168.100.105:8000");
+
+        socket.on('connected',(data)=>{
+            console.log(4444444555555)
+            socket.emit('setAdmin',{name:55})
+        })
+
+        socket.on('message',(data)=>{
+            console.log('message');
+            //this.props.shenList()
+        })
+    }
+
+    _onMessageWasSent(message) {
+        this.setState({
+            messageList: [...this.state.messageList, message]
+        })
+    }
+
+    _sendMessage(text) {
+        if (text.length > 0) {
+            this.setState({
+                messageList: [...this.state.messageList, {
+                    author: 'them',
+                    type: 'text',
+                    data: { text }
+                }]
+            })
+        }
+    }
+
     render(){
         return(
-            <div>
-                <ChatBot steps={steps} />
-            </div>
+            <Launcher
+                agentProfile={{
+                    teamName: 'react-live-chat',
+                    imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
+                }}
+                onMessageWasSent={this._onMessageWasSent.bind(this)}
+                messageList={this.state.messageList}
+                showEmoji
+            />
         );
     }
 
